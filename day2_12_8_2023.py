7cb77aa7fd55e6829e489c37e8b57ec88ce435d6
Aaron D'Costa <dcostaaaron@yahoo.in>
2023-12-08 12:54:50 -0800

    Update day2_12_8_2023.py

--- day2_12_8_2023.py
+++ day2_12_8_2023.py
@@ -1,1 +1,10 @@
+#https://www.youtube.com/watch?v=_shVnvlxufQ  ~ bulb switcher problem
 
+import math
+n = int(input())
+print(int(math.sqrt(n))
+
+#https://www.youtube.com/results?search_query=leetcode+medium+problems+python ~ Practice Leetcode problems
+
+#some learning Cracking the coding interview 
+#https://www.metacareers.com/profile/preparation_hub?c=3167558840219655&chooseView=Hello+World
